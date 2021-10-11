0c0ecb505d851939d60d0c5e0a57c25a6e167750
mjacoupy <maxime.jacoupy@gmail.com>
2021-10-11 13:56:04 +0200

    mm

--- titanic.py
+++ titanic.py
@@ -11,6 +11,7 @@ Created on Thu Oct  7 15:31:10 2021
 import pandas as pd
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 
 sns.set_theme()
 # #######################################################################################################################
@@ -38,10 +39,10 @@ test_df = pd.read_csv('input/test.csv')
 
 train_df = train_df.drop(['Name', 'PassengerId', 'Ticket', 'Cabin'], axis=1)
 test_df = test_df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
-train_df['Sex'] = train_df['Sex'].apply(lambda x:1 if x=='male' else 0)
-test_df['Sex'] = test_df['Sex'].apply(lambda x:1 if x=='male' else 0)
-train_df['Embarked'] = test_df['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
-test_df['Embarked'] = test_df['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+train_df['Sex'] = train_df['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+test_df['Sex'] = test_df['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+train_df['Embarked'] = test_df['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+test_df['Embarked'] = test_df['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
 
 
 test_df['Fare'].fillna(test_df['Fare'].dropna().median(), inplace=True)
@@ -56,17 +57,14 @@ if Sex == "Femme":
 elif Sex == "Homme":
     s = 1
     c = "darkblue"
-sub_df = train_df[train_df['Sex']==s]
+sub_df = train_df[train_df['Sex'] == s]
 
+g = plt.figure()
+sns.histplot(data=sub_df,
+    x='Age',
+    color=c,
+    bins=20).set_title("Répartition des décés chez les "+Sex+"s")
 
-g = sns.histplot(data=sub_df, 
-                 x='Age',
-                 color=c,
-                 bins=20)
-
-
-title = "Répartition des décés chez les "+Sex+"s"
-g.set_title(title)
 
 
 
@@ -79,20 +77,19 @@ elif Port == "Queenstown":
 elif Port == "Southampton":
     p = 0
 
-sub_df = train_df[train_df['Embarked']==p]
-sub_df2 = sub_df[sub_df['Pclass']==Class]
+sub_df = train_df[train_df['Embarked'] == p]
+sub_df2 = sub_df[sub_df['Pclass'] == Class]
 
-h = sns.barplot(data=sub_df2, 
-                x='Sex',
-                y='Survived')
-title2 = "Survie pour les passagers de la class "+str(Class)+" étant parti de "+Port
-h.set_title(title2)
+f = plt.figure()
+sns.barplot(data=sub_df2,
+    x='Sex',
+    y='Survived').set_title("Survie pour les passagers de la classe "+str(Class)+" étant parti de "+Port)
 
 
 # Data Prep
 X_train = train_df.drop("Survived", axis=1)
 Y_train = train_df["Survived"]
-X_test  = test_df.drop("PassengerId", axis=1).copy()
+X_test = test_df.drop("PassengerId", axis=1).copy()
 
 random_forest = RandomForestClassifier(n_estimators=10)
 random_forest.fit(X_train, Y_train)
