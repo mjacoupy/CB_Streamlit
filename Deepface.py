b8ebf4c57248b28971864be0308a31363d4c3e2c
mjacoupy <maxime.jacoupy@gmail.com>
2021-11-30 18:01:40 +0100

    new file

--- Deepface.py
+++ Deepface.py
@@ -15,17 +15,16 @@ import matplotlib.pyplot as plt
 
 data = "/Users/maximejacoupy/Downloads/Odile.jpeg"
 
-if data:
-    pil_image = Image.open(data).convert('RGB')
-    open_cv_image = np.array(pil_image)
-    image = cv2.cvtColor(open_cv_image[:, :, ::-1], cv2.COLOR_BGR2RGB)
+pil_image = Image.open(data).convert('RGB')
+open_cv_image = np.array(pil_image)
+image = cv2.cvtColor(open_cv_image[:, :, ::-1], cv2.COLOR_BGR2RGB)
+
+analyse = DeepFace.analyze(image, actions=['age', 'gender', "emotion", "race"])
+age = analyse['age']
+genre = analyse['gender']
+emotion = analyse['dominant_emotion']
+race = analyse['dominant_race']
     
-# Bonus 3. Estime age et genre
-    analyse = DeepFace.analyze(image, actions=['age', 'gender', "emotion", "race"])
-    age = analyse['age']
-    genre = analyse['gender']
-    emotion = analyse['dominant_emotion']
-    race = analyse['dominant_race']
 plt.imshow(image)    
 print(age, genre, emotion, race)    
 
